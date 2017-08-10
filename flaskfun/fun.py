6b4db7b1733bdcecf18468ac70052aacb08e546a
gaderian <rigelerik@gmail.com>
2017-08-10 16:14:13 +0000

    testing some templates

--- flaskfun/fun.py
+++ flaskfun/fun.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello friend!'
+    return '<a href="derp">hello friend!</a>'
+    
+@app.route('/derp')
+def test():
+    e='test this'
+    return render_template('test.html', entry=e)
     
 if __name__ == '__main__':
     app.run()
