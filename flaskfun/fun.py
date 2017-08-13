f6b5019b7b3eb1b3f2b675bc56ea7cedf2591661
gaderian <rigelerik@gmail.com>
2017-08-13 18:56:09 +0000

    trying to upload file

--- flaskfun/fun.py
+++ flaskfun/fun.py
@@ -1,5 +1,12 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '~/Documents/flaskfun/uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'png', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLEDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello():
@@ -10,5 +17,39 @@ def test():
     e='test this'
     return render_template('test.html', entry=e)
     
+    
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def file_upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+            
+    form = '''
+    <!doctype html>
+    <title>Upload file</title>
+    <form method=post enctype=multipart/form-data>
+        <p><input type=file name=file>
+            <input type=submit value=Upload>
+    </form>
+    '''
+    return form
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
